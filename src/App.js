39ae352654faef63fa3ed5f3d69dbffc526dc6c3
ibraheemzeb <ibraheem_zeb@outlook.com>
2023-09-17 17:46:55 +0500

    activities route fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 // import { useSelector } from 'react-redux';
 import "./App.css";
-import React, { lazy,Suspense  } from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import LazyLoader from "./components/utils/LazyLoader";
@@ -16,8 +16,8 @@ const TheSlope = lazy(() => import("./pages/Slope"));
 const Packages = lazy(() => import("./pages/packages"));
 const BlogMain = lazy(() => import("./pages/BlogMain"));
 const Activities = lazy(() => import("./pages/activities"));
-const DineIn = lazy(() => import("./pages/activities"));
-const CSRMain = lazy(() => import('./pages/CSR'));
+const DineIn = lazy(() => import("./pages/DineIn"));
+const CSRMain = lazy(() => import("./pages/CSR"));
 
 function App() {
   // const header = useSelector(state => state.header.visible);
@@ -27,29 +27,29 @@ function App() {
     <>
       {/* <ResponsiveAppBar /> */}
       {/* <AnnouncementBanner/> */}
-        <Router>
-          <Suspense
-            fallback={
-              <>
-                <LazyLoader />
-              </>
-            }
-          >
-            <Routes>
-              <Route exact path="/" element={<HomePage />} />
+      <Router>
+        <Suspense
+          fallback={
+            <>
+              <LazyLoader />
+            </>
+          }
+        >
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
 
-              <Route exact path="/home" element={<HomePage />} />
-              <Route exact path="/index" element={<HomePage />} />
-              <Route exact path="/staycation" element={<Staycation />} />
-              <Route exact path="/the-slope" element={<TheSlope />} />
-              <Route exact path="/packages" element={<Packages />} />
-              <Route exact path="/activities" element={<Activities />} />
-              <Route exact path="/blog" element={<BlogMain />} />
-              <Route exact path="/csr" element={<CSRMain />} />
-              <Route exact path="/dinein" element={<DineIn />} />
-            </Routes>
-          </Suspense>
-        </Router>
+            <Route exact path="/home" element={<HomePage />} />
+            <Route exact path="/index" element={<HomePage />} />
+            <Route exact path="/staycation" element={<Staycation />} />
+            <Route exact path="/the-slope" element={<TheSlope />} />
+            <Route exact path="/packages" element={<Packages />} />
+            <Route exact path="/activities" element={<Activities />} />
+            <Route exact path="/blog" element={<BlogMain />} />
+            <Route exact path="/csr" element={<CSRMain />} />
+            <Route exact path="/dinein" element={<DineIn />} />
+          </Routes>
+        </Suspense>
+      </Router>
     </>
   );
 }
