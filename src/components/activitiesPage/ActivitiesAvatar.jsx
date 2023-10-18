1ed21bab7b615d7c7b75cfc43dbca7b1fbd584f5
ibraheemzeb <ibraheem_zeb@outlook.com>
2023-10-18 22:43:21 +0500

    CSR/DineIn

--- src/components/activitiesPage/ActivitiesAvatar.jsx
+++ src/components/activitiesPage/ActivitiesAvatar.jsx
@@ -15,62 +15,61 @@ const ActivitiesAvatar = () => {
 
   return (
     <>
-        <Container className="activites-cover-2-col">
-          {/* <img src="outdoor.jpeg" alt="Activities" /> */}
+      <Container className="activites-cover-2-col">
+        {/* <img src="outdoor.jpeg" alt="Activities" /> */}
+        <Grid
+          container
+          display="flex !important"
+          justifyContent="center !important"
+        >
           <Grid
-            container
-            display="flex !important"
-            justifyContent="center !important"
+            item
+            md={6}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
           >
-            <Grid
-              item
-              md={6}
+            <Avatar
+              alt="Activities"
+              src="outdoor-min.jpeg"
               sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
+                width: { xs: 250, sm: 400 },
+                height: { xs: 250, sm: 400 },
               }}
-            >
-              <Avatar
-                alt="Activities"
-                src="outdoor-min.jpeg"
-                sx={{
-                  width: { xs: 250, sm: 400 },
-                  height: { xs: 250, sm: 400 },
-                }}
-              />
-            </Grid>
-            <Grid
-              Item
-              md={6}
-              mt={{ xs: 10, sm: 10, md: 10, lg: 5 }}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                textAlign: {
-                  xs: "Center",
-                  sm: "Center",
-                  md: "Center",
-                  lg: "start",
-                },
-              }}
-            >
-              <Stack direction="column">
-                <Typography variant="h4" color="#094e9d">
-                  Adventurous yet Fun Activities <br /> at Malam Jabba Ski
-                  Resort
-                </Typography>
-                <Typography variant="subtitle">
-                  Our exhilarating Chairlift, Ziplining, Giant Swing, Human
-                  Slingshot and kids activities made for fun and memorable
-                  adventures. We strive to enhance the lives of our guests by
-                  creating the best human-powered outdoor adventures available.{" "}
-                </Typography>
-              </Stack>
-            </Grid>
+            />
           </Grid>
-        </Container>
+          <Grid
+            Item
+            md={6}
+            mt={{ xs: 10, sm: 10, md: 10, lg: 5 }}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              textAlign: {
+                xs: "Center",
+                sm: "Center",
+                md: "Center",
+                lg: "start",
+              },
+            }}
+          >
+            <Stack direction="column">
+              <Typography variant="h4" color="#094e9d">
+                Adventurous yet Fun Activities <br /> at Malam Jabba Ski Resort
+              </Typography>
+              <Typography variant="subtitle">
+                Our exhilarating Chairlift, Ziplining, Giant Swing, Human
+                Slingshot and kids activities made for fun and memorable
+                adventures. We strive to enhance the lives of our guests by
+                creating the best human-powered outdoor adventures available.{" "}
+              </Typography>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 };
