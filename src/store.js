934e6d92d0c930760868c42f75c960df8078da7b
Nashit <nashitquddus1@gmail.com>
2023-10-18 14:46:20 +0500

    vite-conversion step 1

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
-import headerReducer from './components/header/HeaderSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import headerReducer from "./components/header/HeaderSlice";
 
 export const store = configureStore({
-  reducer: {header: headerReducer},
-})
+  reducer: { header: headerReducer },
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+// export type RootState = ReturnType<typeof store.getState>;
+// console.log(typeof store.getState, "TYPE");
+// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+// export type AppDispatch = typeof store.dispatch;
